4d52f426d96a64402f24b8d3edc9c1dc5738a91a
GauravDhavale <Gauravdhavale@gmail.com>
2017-06-18 03:20:12 -0500

    Flight Schedule logic updated

--- assignment_01/create_flight_schedule.py
+++ assignment_01/create_flight_schedule.py
@@ -28,7 +28,7 @@ gatedtl = [['B', 'A', 'G1', 'AUS', 000, 000],
 #gate values similar for same airport to avoide within airport flight booking
 gateValue = {'G1':1, 'G2':2, 'G3':2, 'G4':3, 'G5':3, 'G6':3}
 # Tail number, from Gate , from airport, To Gate, to airport
-bookFlight = ['', '','','','']
+bookFlight = ['A', 'A','A','A','A']
 
 airport = ['AUS', 'DAL', 'HOU']
 flight_times = {'AUS-DAL': 50, 'DAL-AUS': 50, 'AUS-HOU' : 45, 'HOU-AUS' : 45, 'DAL-HOU': 65, 'HOU-DAL' : 65}
@@ -38,7 +38,8 @@ airport_gate = [['AUS', 1], ['DAL', 2], ['HOU', 3]]
 airportGates = {'G1': 'AUS', 'G2':'DAL', 'G3':'DAL', 'G4': 'HOU', 'G5':'HOU', 'G6':'HOU'}
 airport_gate_no = dict(airport_gate)
 flight_schedule = []
-final_flight_schedule = [['tail_number','origin','destination','departure_time','arrival_time']]
+final_flight_schedule = [['tail_number','origin','destination','departure_time','arrival_time', 
+                          'From Gate', 'To Gate']]
 start_time =360
 timer = 360
 end_time = 1320
@@ -51,9 +52,10 @@ def minutesSinceMidntToTime(mTime):
 """Function to print the flight schedule using list"""
 def printFlightScedule():
     import csv
-    sortFlightList()
+    #sortFlightList()
     for row in flight_schedule:
         final_flight_schedule.append(row)
+    #print(final_flight_schedule)
     with open('flight_schedule.csv', 'w', newline='') as schedule:
          wr = csv.writer(schedule, quoting=csv.QUOTE_ALL)
          for item in final_flight_schedule:
@@ -97,7 +99,7 @@ def searchGate():
                 return gate[2]
             else:                
                 arrivalTime = timer + flight_times[bookFlight[2]+'-'+airportGates[gate[2]]]
-                if arrivalTime < gate[4] or arrivalTime > gate[5]:
+                if arrivalTime > gate[5]:
                     return gate[2]
     return 'NG' # in case if no gate is availale
 
@@ -106,47 +108,63 @@ def updateBFBTForFlightAndGate():
     #update flight
     for item in aircraftdtl:
         if item[0] == bookFlight[0]:
-            item[2] = airportGates[bookFlight[1]] # flight is on which gate 
-            item[4] = timer #flight booked from
+            item[2] = bookFlight[1] # flight is on which gate 
+            item[3] = timer #flight booked from
             # timer + flight travel time + minimum wait time : # flight booked till
             item[4] = timer + flight_times[bookFlight[2]+'-'+bookFlight[4]] + airport_wait_time[bookFlight[4]]    
+            print(item)
     # update gate
-    for item in gatedtl:
-        if item[2] == bookFlight[2]:
+    for item in gatedtl:        
+        if item[2] == bookFlight[3]:
             # Gate will bbe busy from flight landing time till min airport wait time
             item[4] = timer + flight_times[bookFlight[2]+'-'+bookFlight[4]] 
-            item[5] = timer + flight_times[bookFlight[2]+'-'+bookFlight[4]] + airport_wait_time[bookFlight[4]]   
+            item[5] = timer + flight_times[bookFlight[2]+'-'+bookFlight[4]] + airport_wait_time[bookFlight[4]]
+            print(item)
 
 # Add flight entry into flight_schedule list
 def updateFlightScheduleList():
     arrivaltime = timer + flight_times[bookFlight[2]+'-'+bookFlight[4]]
-    row = [bookFlight[0],bookFlight[2],bookFlight[4], minutesSinceMidntToTime(timer), minutesSinceMidntToTime(arrivaltime)]
+    row = [bookFlight[0],bookFlight[2],bookFlight[4], minutesSinceMidntToTime(timer), 
+           minutesSinceMidntToTime(arrivaltime),bookFlight[1], bookFlight[3],timer,arrivaltime]
     flight_schedule.append(row)
+    #print('row', row)
+
+#increment global timer
+def incrementTimer():
+    global timer
+    timer = timer+1
     
 """Flight Algorithm """
 def prepareFlightSchedule():
-    for timer in range(start_time, end_time):
-        resetFlightAvailability()#reset flight status for new time check            
+    while timer < end_time:
+        resetFlightAvailability()#reset flight status for new time check
+        resetGateAvailability() #reset gate status for new time check            
         for flight in aircraftdtl:
+            #print(timer)
             if(flight[1] == 'A'):
                 bookFlight[0] = flight[0]
-                if flight[2] == 'STN':            
-                    resetGateAvailability() #reset gate status for new time check
+                print('flight', flight[0])
+                if flight[2] == 'STN':                                
                     for item in gatedtl:
                         if(item[1] == 'A' and item[0] == 'B'):
                             bookFlight[1] = item[2]
                             bookFlight[2] = airportGates[item[2]]
+                            print(item[2], airportGates[item[2]])
                             item[1] = 'B'
                             secondGate = searchGate()
+                            #print('second', secondGate)
                             if secondGate != 'NG':
                                 bookFlight[3] = secondGate
                                 bookFlight[4] = airportGates[secondGate]
+                                print('second gate', secondGate, airportGates[secondGate])
                                 # found fligh and both gates now update BF, BT and Flight Schedule
+                                flight[1] = 'B'
+                                updateBFBTForFlightAndGate()
                                 updateFlightScheduleList()
+                                break
                 else:
                     bookFlight[1] = flight[2]
                     bookFlight[2] = airportGates[flight[2]]
-                    resetGateAvailability() #reset gate status for new time check
                     #now mark this gate as busy
                     for gate in gatedtl:
                         if gate[2] == flight[2]:
@@ -157,16 +175,12 @@ def prepareFlightSchedule():
                         bookFlight[3] = secondGate
                         bookFlight[4] = airportGates[secondGate]
                         # found fligh and both gates now update BF, BT and Flight Schedule
+                        flight[1] = 'B'
+                        updateBFBTForFlightAndGate()
                         updateFlightScheduleList()
+                        break
+        incrementTimer() 
     printFlightScedule()
+    #print(flight_schedule)
                             
 prepareFlightSchedule()
-    
-
-
-
-   
-
-    
-
- 
