c8bc3dc42df6c4d324d56b0abf12d21839b6931d
GauravDhavale <Gauravdhavale@gmail.com>
2017-08-07 23:56:03 -0500

    logic updated to print counts of events and tweets

--- assignment_04/twitter_analytics.py
+++ assignment_04/twitter_analytics.py
@@ -5,3 +5,20 @@ Created on Sun Aug  6 16:08:26 2017
 @author: Gaurav
 """
 
+import requests
+# url for training data set
+url_JSON_Events = 'http://kevincrook.com/utd/tweets.json'
+json_data = {}
+#This function is used to access the dataset URL, fetch the data and load it into the text file
+def readTweets():
+    r = requests.get(url_JSON_Events)
+    frequency_counter = 0
+    json_data = r.json()
+    with open("twitter_analytics.txt", "w") as txt:
+        for item in json_data:
+            if 'text' in item:
+                frequency_counter += 1
+        print(len(json_data), file = txt)
+        print(frequency_counter, file = txt)
+    
+readTweets()
