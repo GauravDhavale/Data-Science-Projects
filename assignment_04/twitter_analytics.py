afa85c33882209ee6fed64313c8947ec42275593
GauravDhavale <Gauravdhavale@gmail.com>
2017-08-08 20:08:37 -0500

    logic updated for tweet count group by language

--- assignment_04/twitter_analytics.py
+++ assignment_04/twitter_analytics.py
@@ -6,6 +6,7 @@ Created on Sun Aug  6 16:08:26 2017
 """
 
 import requests
+import pandas as pd
 # url for training data set
 url_JSON_Events = 'http://kevincrook.com/utd/tweets.json'
 json_data = {}
@@ -14,11 +15,19 @@ def readTweets():
     r = requests.get(url_JSON_Events)
     frequency_counter = 0
     json_data = r.json()
-    with open("twitter_analytics.txt", "w") as txt:
+    df = pd.DataFrame(json_data)
+    df_grp = df.groupby(['lang'])[["text"]].count()
+    df_grp_sorted = df_grp.sort_values('text', ascending=False)
+    df_string = df_grp_sorted.to_string(header=False, index_names=False).split('\n')
+    df_list_string = [','.join(item.split()) for item in df_string]
+    with open("twitter_analytics.txt", "w", encoding = 'UTF-8') as txt:
         for item in json_data:
             if 'text' in item:
                 frequency_counter += 1
         print(len(json_data), file = txt)
         print(frequency_counter, file = txt)
+        for item in df_list_string:
+            print(item, file = txt)
+        
     
 readTweets()
